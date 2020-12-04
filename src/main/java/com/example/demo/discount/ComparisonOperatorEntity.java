a05a601b97e9bb7a75dfa9a299064f661a2547ef
ballyang747 <561902273@qq.com>
2020-12-04 08:16:12 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/discount/ComparisonOperatorEntity.java
@@ -0,0 +1,25 @@
+package com.example.demo.discount;
+
+import java.util.List;
+
+public class ComparisonOperatorEntity {
+
+    private String names;
+    private List<String> list;
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+}
---
+++ src/main/java/com/example/demo/test222.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+
+import com.example.demo.discount.ComparisonOperatorEntity;
+import com.example.demo.discount.Order;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class test222 {
+    @Test
+    public  void test11(){
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieClasspathContainer = kieServices.getKieClasspathContainer();
+        //会话对象，用于和规则引擎交互
+        KieSession kieSession = kieClasspathContainer.newKieSession();
+
+        //构造订单对象，设置原始价格，由规则引擎根据优惠规则计算优惠后的价格
+        Order order = new Order();
+        order.setOriginalPrice(210D);
+
+        //将数据提供给规则引擎，规则引擎会根据提供的数据进行规则匹配
+        kieSession.insert(order);
+
+        //激活规则引擎，如果规则匹配成功则执行规则
+        kieSession.fireAllRules();
+        //关闭会话
+        kieSession.dispose();
+
+        System.out.println("优惠前原始价格：" + order.getOriginalPrice() +
+                "，优惠后价格：" + order.getRealPrice());
+    }
+
+    @Test
+    public  void test22(){
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieClasspathContainer = kieServices.getKieClasspathContainer();
+        //会话对象，用于和规则引擎交互
+        KieSession kieSession = kieClasspathContainer.newKieSession();
+
+        //构造订单对象，设置原始价格，由规则引擎根据优惠规则计算优惠后的价格
+        ComparisonOperatorEntity order = new ComparisonOperatorEntity();
+        order.setNames("aaaaa");
+        //将数据提供给规则引擎，规则引擎会根据提供的数据进行规则匹配
+        kieSession.insert(order);
+
+        //激活规则引擎，如果规则匹配成功则执行规则
+        kieSession.fireAllRules();
+        //关闭会话
+        kieSession.dispose();
+
+
+    }
+}
