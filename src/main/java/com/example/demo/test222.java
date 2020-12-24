c0ea09138f340a7861e1146756b5dec28fdbc96e
ballyang747 <561902273@qq.com>
2020-12-24 08:32:09 +0800

    ll

--- src/main/java/com/example/demo/test222.java
+++ src/main/java/com/example/demo/test222.java
@@ -23,17 +23,22 @@ public class test222 {
         KieContainer kieClasspathContainer = kieServices.getKieClasspathContainer();
         //会话对象，用于和规则引擎交互
         KieSession kieSession = kieClasspathContainer.newKieSession();
-        List aaa = new ArrayList();
+        List<Order> aaa = new ArrayList();
         //构造订单对象，设置原始价格，由规则引擎根据优惠规则计算优惠后的价格
         Order order = new Order();
 
         order.setRemark("CAAAADSADD");
         order.setRealPrice(555.454444);
+
+        Order order2 = new Order();
+
+        order2.setRemark("CAAAADSADD");
+        order2.setRealPrice(556.454444);
         aaa.add(order);
         //aaa.add(ss);
         //将数据提供给规则引擎，规则引擎会根据提供的数据进行规则匹配
         kieSession.insert(order);
-
+        kieSession.insert(order2);
         //激活规则引擎，如果规则匹配成功则执行规则
         kieSession.fireAllRules();
         //关闭会话
